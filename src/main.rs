c875cb9b0bd2f0e2b16a4dc951eb93ce3cae7113
Timothy Parrish <timothy.parrish@dish.com>
2024-04-01 16:34:57 -0600

    Basic webserver with thread pooling

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -22,12 +28,14 @@ fn handle_connection(mut stream: TcpStream) {
         .unwrap()
         .unwrap();
 
-    let (status_line, filename) =
-        if request_line == "GET / HTTP/1.1" {
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "html/hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
             ("HTTP/1.1 200 OK", "html/hello.html")
-        } else {
-            ("HTTP/1.1 404 NOT FOUND", "html/404.html")
-        };
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "html/404.html"),
+    };
     
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
